92a6d0323c247a844d3e0dcd542869b29891dff1
Marcos157-byte <cmpaolo8@gmail.com>
2026-04-15 19:56:09 -0500

    Clase de seminario de integracion 2026-04-15

--- modulo_1/python/03_strings.py
+++ modulo_1/python/03_strings.py
@@ -13,4 +13,11 @@ print("Uno", "Dos", "Tres",  end=" | ")
 nombre="Maria Ramos"
 edad=29
 print(nombre, edad)
+print(f"Nombre:{nombre}, {edad}")
+print(f"Doble de edad: {edad}, {edad*2}")
+print(f"{'Maria' :>5}")
 
+pi=3.14159
+
+print(f"{pi:.2f}")
+print(f"{1000000:,}")
---
+++ modulo_1/python/04_input.py
@@ -0,0 +1,8 @@
+nombre=input("Cual es tu nombre")
+print(f"Hola {nombre}")
+
+
+edad_str=input("Cual es tu edad")
+print(f"Tu edad es {edad_str}")
+edad_int=int(edad_str)
+print(f"El doble de tu edad es {edad_int*2}")
