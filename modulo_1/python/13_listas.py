791d3aafe7aba71451d1b7ecf47d38ba0f39612e
Marcos157-byte <cmpaolo8@gmail.com>
2026-04-29 20:00:11 -0500

    Clase 29-04-2026

---
+++ modulo_1/python/13_listas.py
@@ -0,0 +1,18 @@
+print("Manupulacion de listas")
+print("Crear listas")
+
+vacia= []
+print(vacia)
+numeros= [1,2,3,4,5]
+print(numeros)
+numbres= ["Juan","Juanan","Jose","Yamil"]
+print(numbres)
+mixta= [1,3,"Hello",True,"Wold",None,3.14]
+print(mixta)
+anidada= [1,2,[3,4],5]
+print(anidada)
+print("Acceder a elementos de la lista")
+print(numeros[1])
+print(numbres[-1])
+
+print("Crud de listas")
---
+++ modulo_1/python/14_diccionario.py
@@ -0,0 +1,7 @@
+print("Diccionarios")
+
+vacio = {}
+persona = {"nombre": "Juan", "edad": 30, "ciudad": "Quito"}
+print(vacio)
+print(persona)
+config= dict(host="localhost", port=8080, debug=True)
