4d5c3c52250283e7dedc4027f21e2057c6997ce3
Marcos157-byte <cmpaolo8@gmail.com>
2026-07-01 20:02:41 -0500

    Clae 01/07/2027

---
+++ modulo_4/01_guion_componentes/src/App.tsx
@@ -0,0 +1,10 @@
+// src/App.tsx
+
+export default function App() {
+  return (
+    <main style={{ maxWidth: 480, margin: '40px auto', fontFamily: 'sans-serif' }}>
+      <h1>Hola desde React 19 + TypeScript</h1>
+      <p>Proyecto configurado con Vite 8.</p>
+    </main>
+  )
+}
