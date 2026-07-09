7eacd31a8642d6b5b4bf8c9e5168d2cf82ce956c
Marcos157-byte <cmpaolo8@gmail.com>
2026-07-08 20:03:53 -0500

    Clase 08-07-2026

--- modulo_4/01_guion_componentes/src/main.tsx
+++ modulo_4/01_guion_componentes/src/main.tsx
@@ -1,11 +1,18 @@
 // src/main.tsx
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from './useContext/ThemeContext.tsx'
+import { AuthProvider }  from './useContext/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
